b70be1c52a96b400c36de5e528543d51de897561
Jolita <jolita@test.lt>
2023-04-24 20:18:40 +0300

    Box component created

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,30 @@
-import { lazy } from 'react';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import PageLayout from './pages/PageLayout';
+import { lazy } from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import PageLayout from "./pages/PageLayout";
+import Box from "./components/atoms/Box";
 
-const HomePage = lazy(() => import('./pages/HomePage'));
-const TodoListPage = lazy(() => import('./pages/TodoListPage'));
+const HomePage = lazy(() => import("./pages/HomePage"));
+const TodoListPage = lazy(() => import("./pages/TodoListPage"));
 
 function App() {
   const router = createBrowserRouter([
     {
-      path: '/',
+      path: "/",
       element: <PageLayout />,
       children: [
         {
-          path: '',
+          path: "",
           element: <HomePage />,
         },
         {
-          path: 'todos',
+          path: "todos",
           element: <TodoListPage />,
         },
       ],
     },
   ]);
 
-  return <RouterProvider router={router} />;
+  return;
 }
 
 export default App;
